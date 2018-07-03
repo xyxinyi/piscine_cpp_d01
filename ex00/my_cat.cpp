a4a39b3450cb6ff5d91aa1a47c77bb763b8e7be6
xinyi <524950570@qq.com>
2018-07-03 08:35:16 +0800

    two

--- ex00/my_cat.cpp
+++ ex00/my_cat.cpp
@@ -3,19 +3,14 @@
 using namespace std;
 
 void printFile(char *filename){
-	//opens file
 	ifstream infile(filename);
-		//if file exsists
 		if (!infile.fail()){
-			//print each line of file character by character
 			char ch;
 			while (infile.get(ch)) {
 				cout << ch;
 			}
-			//close opened file
 			infile.close();
 		}
-		// if file isnt found give error message
 		else {
 					cerr << "cat: " << filename << ": no such file or directory" << "\n";
 			}
@@ -24,11 +19,9 @@ void printFile(char *filename){
 
 
 int main(int argc, char *argv[]) {
-	// if no arguements are passed return nothing
 	if (argc == 1){
 		return 0;
 	}
-	//if they are call print file to print arguements
 	else {
 		for (int i = 1;i < argc;i++){
 		printFile(argv[i]);
